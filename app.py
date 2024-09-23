73de4dde9dcdcdc353084a4e1ec2889f1bed6281
Jay Singh Chauhan <jjchauhan@outlook.com>
2024-09-23 01:27:32 -0400

    changed output tables and added finite differencing support

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 from flask import Flask, render_template, request
 import numpy as np
+import pandas as pd
 from src.option import European_Option, American_Option
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET','POST'])
 def calculate_price():
+    # Adding tooltips for each Greek
+    tooltips = {
+        'Stock Price': 'Initial stock price',
+        'Option Value': 'Option value based on the chosen model',
+        'Delta 𝚫': 'Rate of change of option value with respect to the underlying asset price',
+        'Gamma 𝚪': 'Rate of change of delta with respect to the underlying asset price',
+        'Theta 𝚯': 'Rate of change of option value with respect to time',
+        'Vega 𝓋': 'Option sensitivity to volatility of the underlying asset',
+        'Rho 𝛒': 'Option sensitivity to interest rate changes'
+    }
+
     if request.method == 'POST':
         S = float(request.form['stock_price'])
         K = float(request.form['strike_price'])
@@ -26,11 +38,11 @@ def calculate_price():
             seed = int(request.form['seed'])
             n = int(request.form['iterations'])
             dt = float(request.form['timestep'])
-            SO.setSeedVariables(seed=seed,n=n,dt=dt)
+            SO.setSeedVariables(seed=seed, n=n, dt=dt)
         elif method == 'LSMC':
             seed = int(request.form['seed'])
             n = int(request.form['iterations'])
-            SO.setSeedVariables(seed=seed,n=n)
+            SO.setSeedVariables(seed=seed, n=n)
         elif method in ['BT','TT']:
             n = int(request.form['time_steps'])
             SO.setTreeSteps(n=n)
@@ -38,10 +50,52 @@ def calculate_price():
             S_min = float(request.form['stock_min_price'])
             S_max = float(request.form['stock_max_price'])
             dt = float(request.form['timestep'])
-            SO.setFDVariables(S_min=S_min,S_max=S_max,dt=dt)
+            SO.setFDVariables(S_min=S_min, S_max=S_max, dt=dt)
+
+            res = SO.priceOption()
+            res_data = {
+                'Stock Price': res['stock_price'], 
+                'Option Value': res['price'], 
+                'Delta - 𝚫': res['delta'], 
+                'Gamma - 𝚪': res['gamma'], 
+                'Theta - 𝛳': res['theta'], 
+                'Vega - 𝓋': res['vega'], 
+                'Rho - 𝛒': res['rho']
+            }
+            df = pd.DataFrame(res_data).sort_values(by='Stock Price')
+            res_table_html = df.to_html(
+                classes='table table-hover table-striped table-bordered',
+                index=False,
+                escape=False,
+                header=True
+            )
+
+            # Render the HTML table in the template
+            return render_template('index.html', res_table_html=res_table_html, tooltips=tooltips)
 
         res = SO.priceOption()
-        return render_template('index.html',res=res)
+        # return render_template('index.html',res=res)
+
+        # Create a pandas DataFrame for the results, using the metrics as columns
+        res_data = {
+            'Option Value': [res['price']],
+            'Delta - 𝚫': [res['delta']],
+            'Gamma - 𝚪': [res['gamma']],
+            'Theta - 𝛳': [res['theta']],
+            'Vega - 𝓋': [res['vega']],
+            'Rho - 𝛒': [res['rho']],
+        }
+        df = pd.DataFrame(res_data)
+
+        # Convert DataFrame to HTML without row index
+        res_table_html = df.to_html(
+            classes='table table-hover table-striped table-bordered',
+            index=False,
+            escape=False,
+        )
+
+        # Render the HTML table in the template
+        return render_template('index.html', res_table_html=res_table_html, tooltips=tooltips)
     else:
         return render_template('index.html')
 
--- src/option.py
+++ src/option.py
@@ -37,46 +37,61 @@ class Option:
         self.S_max = S_max
         self.dt = dt
 
-    def priceOption(self, eps=0.5):
+    def priceOption(self, eps=1):
         if self.method_name == 'BSM':
             return self.method()
         
         time_eps = 0.05
-        sig_eps = eps / 100
+        sig_eps = eps / 200
+        S = []
 
-        O = self.method()
-        self.S0 += eps
-        Op = self.method()
-        self.S0 -= 2 * eps
-        Om = self.method()
-        self.S0 += eps
+        if self.method_name != 'FD':
+            V = self.method()
+            self.S0 += eps
+            Vp = self.method()
+            self.S0 -= 2 * eps
+            Vm = self.method()
+            self.S0 += eps
+        else:
+            V, S = self.method(return_S = True)
+            self.S_min += eps
+            self.S_max += eps
+            Vp = self.method()
+            self.S_min -= 2 * eps
+            self.S_max -= 2 * eps
+            Vm = self.method()
+            self.S_max += eps
+            self.S_min += eps
 
-        delta = (Op - Om) / 2 / eps
-        gamma = (Op + Om - 2 * O) / (eps**2)
+        delta = (Vp - Vm) / 2 / eps
+        gamma = (Vp + Vm - 2 * V) / (eps**2)
 
         self.T += time_eps
-        OTp = self.method()
+        VTp = self.method()
         self.T -= time_eps
-        theta = (O - OTp) / time_eps
+        theta = (V - VTp) / time_eps
 
         self.sig += sig_eps
-        OSigp = self.method()
+        VSigp = self.method()
         self.sig -= sig_eps
-        vega = (OSigp - O) / sig_eps
+        vega = (VSigp - V) / sig_eps
 
         self.r += sig_eps
-        Orp = self.method()
+        Vrp = self.method()
         self.r -= sig_eps
-        rho = (Orp - O) / sig_eps 
+        rho = (Vrp - V) / sig_eps
 
-        return {"price": round(O, 2), "delta": round(delta, 2), "gamma": round(gamma, 2), "theta": round(theta, 2), "vega": round(vega, 2), "rho": round(rho, 2)}
+        if len(S) > 0:
+            return {"stock_price": S, "price": np.round(V, 3), "delta": np.round(delta, 3), "gamma": np.round(gamma, 3), "theta": np.round(theta, 3), "vega": np.round(vega, 3), "rho": np.round(rho, 3)}
+
+        return {"price": np.round(V, 3), "delta": np.round(delta, 3), "gamma": np.round(gamma, 3), "theta": np.round(theta, 3), "vega": np.round(vega, 3), "rho": np.round(rho, 3)}
 
 class European_Option(Option):
     def BSM(self):
         zp = (np.log(self.S0 / self.K) + (self.r - self.y) * self.T) / (self.sig * np.sqrt(self.T)) + self.sig * np.sqrt(self.T) / 2
         zm = (np.log(self.S0 / self.K) + (self.r - self.y) * self.T) / (self.sig * np.sqrt(self.T)) - self.sig * np.sqrt(self.T) / 2
 
-        O = self.phi * (self.S0 * np.exp(- self.y * self.T) * N(self.phi * zp) - self.K * np.exp(- self.r * self.T) * N(self.phi * zm))
+        V = self.phi * (self.S0 * np.exp(- self.y * self.T) * N(self.phi * zp) - self.K * np.exp(- self.r * self.T) * N(self.phi * zm))
 
         delta = self.phi * np.exp(- self.y * self.T) * N(self.phi * zp)
         rho = self.phi * self.K * self.T * np.exp(- self.r * self.T) * N(self.phi * zm)
@@ -84,7 +99,7 @@ class European_Option(Option):
         deltaK = - self.phi * np.exp(- self.r * self.T) * N(self.phi * zm)
         theta = self.r * self.K * deltaK + self.y * self.S0 * delta - self.sig**2 * self.S0**2 * gamma / 2
         vega = self.S0 * np.sqrt(self.T) * np.exp(- self.r * self.T) * n(zm)
-        return {"price": round(O, 2), "delta": round(delta, 2), "gamma": round(gamma, 2), "theta": round(theta, 2), "vega": round(vega, 2), "rho": round(rho, 2)}
+        return {"price": round(V, 3), "delta": round(delta, 3), "gamma": round(gamma, 3), "theta": round(theta, 3), "vega": round(vega, 3), "rho": round(rho, 3)}
     
     def MC(self):
         m = int(self.T / self.dt)
@@ -103,12 +118,12 @@ class European_Option(Option):
             ST2[:,i+1] = ST2[:,i] * np.exp(mu * self.dt - self.sig * np.sqrt(self.dt) * z[:,i])
 
         if self.option_type == 'call':
-            OTa = (np.where(ST1[:,-1] < self.K, 0, ST1[:,-1] - self.K) + np.where(ST2[:,-1] < self.K, 0, ST2[:,-1] - self.K)) / 2
+            VTa = (np.where(ST1[:,-1] < self.K, 0, ST1[:,-1] - self.K) + np.where(ST2[:,-1] < self.K, 0, ST2[:,-1] - self.K)) / 2
         else:
-            OTa = (np.where(ST1[:,-1] > self.K, 0, self.K - ST1[:,-1]) + np.where(ST2[:,-1] > self.K, 0, self.K - ST2[:,-1])) / 2
+            VTa = (np.where(ST1[:,-1] > self.K, 0, self.K - ST1[:,-1]) + np.where(ST2[:,-1] > self.K, 0, self.K - ST2[:,-1])) / 2
 
-        Ota_est = np.exp(-self.r * self.T) * OTa.mean()
-        return Ota_est
+        Vta_est = np.exp(-self.r * self.T) * VTa.mean()
+        return Vta_est
     
     def BT(self):
         dt = self.T / self.n
@@ -119,14 +134,14 @@ class European_Option(Option):
 
         ST = np.array([self.S0 * u**(self.n - i) * d**i for i in range(self.n + 1)])
         
-        O = np.maximum(self.phi * (ST - self.K), 0)
+        V = np.maximum(self.phi * (ST - self.K), 0)
 
         for i in range(self.n - 1, -1, -1):
-            Ot = np.zeros(i+1)
+            Vt = np.zeros(i+1)
             for j in range(i+1):
-                Ot[j] = np.exp(-self.r * dt) * (p * O[j] + (1-p) * O[j+1])
-            O = Ot
-        return O[0]
+                Vt[j] = np.exp(-self.r * dt) * (p * V[j] + (1-p) * V[j+1])
+            V = Vt
+        return V[0]
     
     def TT(self):
         dt = self.T / self.n
@@ -140,14 +155,64 @@ class European_Option(Option):
         pm = 1 - pd - pu
 
         ST = np.array([self.S0 * u**max(self.n - i, 0) * d**max(i - self.n, 0) for i in range(2 * self.n + 1)])
-        O = np.maximum(self.phi * (ST - self.K), 0)
+        V = np.maximum(self.phi * (ST - self.K), 0)
 
         for i in range(self.n - 1, -1, -1):
-            Ot = np.zeros(2 * i + 1)
+            Vt = np.zeros(2 * i + 1)
             for j in range(2 * i + 1):
-                Ot[j] = np.exp(-self.r * dt) * (pu * O[j] + pm * O[j+1] + pd * O[j+2])
-            O = Ot
-        return O[0]
+                Vt[j] = np.exp(-self.r * dt) * (pu * V[j] + pm * V[j+1] + pd * V[j+2])
+            V = Vt
+        return V[0]
+    
+    def FD(self, return_S = False):
+        M = int(self.T / self.dt)
+        dS = 1
+        alpha = 0.5
+
+        N = int(np.round((self.S_max - self.S_min) / dS) + 1)
+        S = np.linspace(self.S_max, self.S_min, N)
+        j = S / dS
+
+        V = np.zeros((N, M))
+        V[:, -1] = np.maximum(self.phi * (S - self.K), 0)
+
+        a1 = (self.sig**2 * j**2 + (self.r - self.y) * j) * (1 - alpha) * self.dt / 2
+        a2 = - 1 - (self.sig**2 * j**2 + self.r) * (1 - alpha) * self.dt
+        a3 = (self.sig**2 * j**2 - (self.r - self.y) * j) * (1 - alpha) * self.dt / 2
+
+        b1 = - ((self.r - self.y) * j + self.sig**2 * j**2) * alpha * self.dt / 2
+        b2 = (self.sig**2 * j**2 + self.r) * alpha * self.dt - 1
+        b3 = ((self.r - self.y) * j - self.sig**2 * j**2) * alpha * self.dt / 2
+
+        RA = np.zeros((N,N))
+        LA = np.zeros((N,N))
+
+        LA[0, 0] = self.phi
+        LA[0, 1] = -self.phi
+        LA[-1, -1] = -self.phi
+        LA[-1, -2] = self.phi
+
+        for i in range(1, N - 1):
+            LA[i, i - 1] = a1[i]
+            LA[i, i] = a2[i]
+            LA[i, i + 1] = a3[i]
+
+            RA[i, i - 1] = b1[i]
+            RA[i, i] = b2[i]
+            RA[i, i + 1] = b3[i]
+
+        B = np.zeros(N)
+        if self.phi == 1:
+            B[0] = S[0] - S[1]
+        else:
+            B[-1] = S[-2] - S[-1]
+
+        for i in range(M-2,-1,-1):
+            V[:, i] = np.linalg.solve(LA, np.dot(RA, V[:, i + 1]) + B)
+        
+        if return_S:
+            return V[:, 0], S
+        return V[:, 0]
         
 class American_Option(Option):
     def BT(self):
@@ -159,16 +224,16 @@ class American_Option(Option):
 
         ST = np.array([self.S0 * u**(self.n - i) * d**i for i in range(self.n + 1)])
         
-        O = np.maximum(self.phi * (ST - self.K), 0)
+        V = np.maximum(self.phi * (ST - self.K), 0)
 
         for i in range(self.n - 1, -1, -1):
-            Ot = np.zeros(i+1)
+            Vt = np.zeros(i+1)
             St = np.array([self.S0 * u**(i - k) * d**k for k in range(i+1)])
             ev = np.maximum(self.phi * (St - self.K),0)
             for j in range(i+1):
-                Ot[j] = max(ev[j], np.exp(- self.r * dt) * (p * O[j] + (1-p) * O[j+1]))
-            O = Ot
-        return O[0]
+                Vt[j] = max(ev[j], np.exp(- self.r * dt) * (p * V[j] + (1-p) * V[j+1]))
+            V = Vt
+        return V[0]
     
     def TT(self):
         dt = self.T / self.n
@@ -182,16 +247,16 @@ class American_Option(Option):
         pm = 1 - pd - pu
 
         ST = np.array([self.S0 * u**max(self.n - i, 0) * d**max(i - self.n, 0) for i in range(2 * self.n + 1)])
-        O = np.maximum(self.phi * (ST - self.K), 0)
+        V = np.maximum(self.phi * (ST - self.K), 0)
 
         for i in range(self.n - 1, -1, -1):
-            Ot = np.zeros(2 * i + 1)
+            Vt = np.zeros(2 * i + 1)
             St = np.array([self.S0 * u**max(i - k, 0) * d**max(k - i, 0) for k in range(2 * i + 1)])
             ev = np.maximum(self.phi * (St - self.K), 0)
             for j in range(2 * i + 1):
-                Ot[j] = max(ev[j], np.exp(-self.r * dt) * (pu * O[j] + pm * O[j+1] + pd * O[j+2]))
-            O = Ot
-        return O[0]
+                Vt[j] = max(ev[j], np.exp(-self.r * dt) * (pu * V[j] + pm * V[j+1] + pd * V[j+2]))
+            V = Vt
+        return V[0]
     
     def LSMC(self):
         m = int(np.sqrt(self.n))
@@ -234,3 +299,54 @@ class American_Option(Option):
 
         V0 = (Index * EV * np.exp(-self.r * np.arange(1, m + 1) * dt)).sum(axis=1)
         return self.K * V0.mean()
+    
+    def FD(self, return_S = False):
+        M = int(self.T / self.dt)
+        dS = 1
+        alpha = 0.5
+
+        N = int(np.round((self.S_max - self.S_min) / dS) + 1)
+        S = np.linspace(self.S_max, self.S_min, N)
+        j = S / dS
+
+        CV = np.zeros((N, M))
+        CV[:, -1] = np.maximum(self.phi * (S - self.K), 0)
+
+        a1 = (self.sig**2 * j**2 + (self.r - self.y) * j) * (1 - alpha) * self.dt / 2
+        a2 = - 1 - (self.sig**2 * j**2 + self.r) * (1 - alpha) * self.dt
+        a3 = (self.sig**2 * j**2 - (self.r - self.y) * j) * (1 - alpha) * self.dt / 2
+
+        b1 = - ((self.r - self.y) * j + self.sig**2 * j**2) * alpha * self.dt / 2
+        b2 = (self.sig**2 * j**2 + self.r) * alpha * self.dt - 1
+        b3 = ((self.r - self.y) * j - self.sig**2 * j**2) * alpha * self.dt / 2
+
+        RA = np.zeros((N,N))
+        LA = np.zeros((N,N))
+
+        LA[0, 0] = self.phi
+        LA[0, 1] = -self.phi
+        LA[-1, -1] = -self.phi
+        LA[-1, -2] = self.phi
+
+        for i in range(1, N - 1):
+            LA[i, i - 1] = a1[i]
+            LA[i, i] = a2[i]
+            LA[i, i + 1] = a3[i]
+
+            RA[i, i - 1] = b1[i]
+            RA[i, i] = b2[i]
+            RA[i, i + 1] = b3[i]
+
+        B = np.zeros(N)
+        if self.phi == 1:
+            B[0] = S[0] - S[1]
+        else:
+            B[-1] = S[-2] - S[-1]
+
+        for i in range(M-2,-1,-1):
+            CV[:, i] = np.linalg.solve(LA, np.dot(RA, CV[:, i + 1]) + B)
+            CV[:, i] = np.maximum(CV[:, i], CV[:, i + 1])
+        
+        if return_S:
+            return CV[:, 0], S
+        return CV[:, 0]
